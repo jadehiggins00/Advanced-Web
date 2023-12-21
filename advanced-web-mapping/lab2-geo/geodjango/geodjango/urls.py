b19454ba05ead1bbe667a8faa8827787abaf5f75
jade <jadehiggins2000@gmail.com>
2023-12-21 17:43:13 +0000

    moved template

--- advanced-web-mapping/lab2-geo/geodjango/geodjango/urls.py
+++ advanced-web-mapping/lab2-geo/geodjango/geodjango/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     # path('assignment1/pine-martens/', pine_martens_view, name='pine-martens'),
     # path('assignment1/bird-watch/', bird_hide_view, name='bird-watch'),
     path('assignment1/signup/', signup, name='signup'),
-     path("api/v1/", include('assignment1.urls')),
+    # path("api/v1/", include('assignment1.urls')),
 
 
     # ... other URL patterns
