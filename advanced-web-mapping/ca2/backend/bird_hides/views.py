29e8ce418297763bdfe7891c62f6f51d59e6a36b
JadeHiggins <jade.higgins@ptools.com>
2023-12-29 03:03:01 +0000

    update,delete and add is done

--- advanced-web-mapping/ca2/backend/bird_hides/views.py
+++ advanced-web-mapping/ca2/backend/bird_hides/views.py
@@ -89,92 +89,18 @@ def manifest(request):
 def offline(request):
     return render(request, "offline.html")
 
-
-# @csrf_exempt
-# def add_bird_location(request):
-#     if request.method == 'POST':
-#         try:
-#             data = json.loads(request.body)
-#             latitude = data['latitude']
-#             longitude = data['longitude']
-#             name = data.get('name', '')  # Optional
-#             description = data.get('description', '')  # Optional
-
-#             location = BirdLocation(latitude=latitude, longitude=longitude, name=name, description=description)
-#             location.save()
-
-#             return JsonResponse({'message': 'Location added successfully!'}, status=200)
-#         except Exception as e:
-#             return JsonResponse({'error': str(e)}, status=400)
-
-
-# views.py
 from django.http import JsonResponse
 
 def get_all_locations(request):
     if request.method == 'GET':
         try:
             locations = BirdLocation.objects.all()
-            data = list(locations.values('latitude', 'longitude', 'name', 'description'))
+            data = list(locations.values('id','latitude', 'longitude', 'name', 'description'))
             return JsonResponse({'locations': data}, status=200)
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=500)
 
 
-
-# class BirdSpotsViewSet(viewsets.ModelViewSet):
-#     queryset = BirdSpots.objects.all()
-#     serializer_class = BirdSpotsSerializer
-
-  
-
-#     def perform_create(self, serializer):
-#         address = serializer.initial_data["address"]
-
-#         try:
-#             g = geolocator.geocode(address)
-#             if g is not None:
-#                 lat = g.latitude
-#                 lng = g.longitude
-#                 pnt = Point(lng, lat)
-#                 serializer.save(location=pnt)
-#             else:
-#                 logger.info(f"Address {address} could not be geocoded.")
-#         except Exception as e:
-#             logger.error(f"Geocoding error: {e}")
-
-
-# class BirdHide(viewsets.ModelViewSet):
-#     queryset = BirdSpots.objects.all()
-#     serializer_class = BirdSpotsSerializer
-
-#     def perform_create(self, serializer):
-#         lat = self.request.data.get('latitude')
-#         lng = self.request.data.get('longitude')
-#         if lat and lng:
-#             location = Point(float(lng), float(lat))
-#             serializer.save(location=location)
-#         else:
-#             serializer.save()
-
-# class BirdHidesUpdateRetreiveView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = BirdHides.objects.all()
-#     serializer_class = BirdHidesSerializer
-
-#     def perform_update(self, serializer):
-#         address = serializer.initial_data["address"]
-#         try: 
-#             g = geolocator.geocode(address)
-#             if g is not None:
-#                 lat = g.latitude
-#                 lng = g.longitude
-#                 pnt = Point(lng, lat)
-#                 print(pnt)
-#                 serializer.save(location=pnt)
-#             else:
-#                 print(f"Address {address} could not be geocoded.")
-#         except Exception as e:
-#             print(f"Geocoding error: {e}")
 
 def fetch_bird_hides_in_ireland(request):
     query = """
--- advanced-web-mapping/ca2/backend/frontend/src/components/BirdHides.js
+++ advanced-web-mapping/ca2/backend/frontend/src/components/BirdHides.js
@@ -1,206 +1,222 @@
-import React, { createRef, Component } from 'react';
+import React, { Component } from 'react';
 import { MapContainer, TileLayer, GeoJSON, Marker, Popup } from 'react-leaflet';
 import osmtogeojson from 'osmtogeojson';
+import L from 'leaflet';
+
 import LegendOrange from '../../static/images/legendOrange.svg';
 import LegendBlue from '../../static/images/legendBlue.svg';
 
-
 export default class BirdHides extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      geojsonData: null,
-      locations: [],
-      showModal: false,
-      currentLocation: null,
-      newLocationName: '',
-      newLocationDescription: '',
-     
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            geojsonData: null,
+            locations: [],
+            showModal: false,
+            currentLocation: null,
+            newLocationName: '',
+            newLocationDescription: '',
+            editingLocationId: null,
+        };
+    }
 
-  customMarkerIcon = L.icon({
-    iconUrl: '/static/images/bio.png', // Replace with your image path
-    iconSize: [35, 35], // Size of the icon
-    iconAnchor: [12, 41], 
-    popupAnchor: [0, -41], 
-  });
-
- 
-  geoJsonStyle = () => {
-    return {
-      color: '#E65100',       
-      weight: 5,          
-      opacity: 1,
-      fillOpacity: 0.7
-    };
-  };
-
-
- // Method to handle button click
- handleAddLocationClick = () => {
-  
-  console.log('clickkk');
-  navigator.geolocation.getCurrentPosition((position) => {
-    this.setState({
-      currentLocation: {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-      },
-      showModal: true
+    customMarkerIcon = L.icon({
+        iconUrl: '/static/images/bio.png', // Replace with your image path
+        iconSize: [35, 35],
+        iconAnchor: [12, 41],
+        popupAnchor: [0, -41],
     });
-  });
-};
 
-// Method to handle form changes
-handleFormChange = (e) => {
-  this.setState({ [e.target.name]: e.target.value });
-};
+    geoJsonStyle = () => {
+        return {
+            color: '#E65100',
+            weight: 5,
+            opacity: 1,
+            fillOpacity: 0.7
+        };
+    };
 
-// Method to submit the new location
-submitNewLocation = () => {
-  const { currentLocation, newLocationName, newLocationDescription } = this.state;
-  const postData = {
-    latitude: currentLocation.latitude,
-    longitude: currentLocation.longitude,
-    name: newLocationName,
-    description: newLocationDescription,
-  };
+    handleAddLocationClick = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            this.setState({
+                currentLocation: {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                },
+                showModal: true,
+                editingLocationId: null
+            });
+        });
+    };
 
-  fetch('/api/add_location/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(postData),
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log('Success:', data);
-    this.setState({ showModal: false });
-  })
-  .catch((error) => {
-    console.error('Error:', error);
+    handleFormChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
 
-  });
+    submitNewLocation = () => {
+        const { currentLocation, newLocationName, newLocationDescription, editingLocationId } = this.state;
+        const url = editingLocationId ? `/api/update_location/${editingLocationId}/` : '/api/add_location/';
+        const method = editingLocationId ? 'PUT' : 'POST';
+
+        const postData = {
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+            name: newLocationName,
+            description: newLocationDescription,
+        };
+
+        fetch(url, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(postData),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Success:', data);
+            this.setState({ showModal: false, editingLocationId: null });
+            this.fetchLocations();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    };
+
+    handleEditLocation = (location) => {
+      debugger;
+        this.setState({
+            currentLocation: {
+                latitude: location.latitude,
+                longitude: location.longitude
+            },
+            newLocationName: location.name,
+            newLocationDescription: location.description,
+            showModal: true,
+            editingLocationId: location.id
+        });
+    };
+
+    handleDeleteLocation = (locationId) => {
+      console.log("Deleting location with ID:", locationId); // Debugging line
+      if (locationId === undefined) {
+          console.error("Error: locationId is undefined");
+          return;
+      }
   
-};
-  
-
-
-  componentDidMount() {
-
-    fetch('/api/birdhides_ireland')
-      .then(response => response.json())
-      .then(osmData => {
-        const geojsonData = osmtogeojson(osmData);
-        this.setState({ geojsonData });
+      fetch(`/api/delete_location/${locationId}/`, {
+          method: 'DELETE',
+      })
+      .then(response => {
+          if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          this.fetchLocations();
       })
       .catch(error => console.error('Error:', error));
-
-    fetch('/api/get_all_locations')
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ locations: data.locations });
-      })
-      .catch(error => console.error('Error:', error));
-
-   
-  }
-
-
-
-
-  onEachFeature = (feature, layer) => {
-    if (feature.properties && feature.properties.name) {
-      layer.bindPopup(feature.properties.name);
-    }
   };
-
-  render() {
-    const { geojsonData, locations } = this.state;
-
-    return (
-      <div>
-        <MapContainer
-          center={[53.4129, -8.2439]}
-          zoom={6}
-          
-        >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; OpenStreetMap contributors'
-            className="grayscale-tiles"
-          />
-       
-          {geojsonData && (
-            <GeoJSON
-              data={geojsonData}
-              onEachFeature={this.onEachFeature}
-              style={this.geoJsonStyle}
-         
-            />
-          )}
-
-     
-          {locations.map((location, index) => (
-            <Marker key={index} position={[location.latitude, location.longitude]} icon={this.customMarkerIcon} >
-              <Popup>
-                {location.name ? <strong>{location.name}</strong> : 'Unknown Location'}
-                <br />
-                {location.description}
-              </Popup>
-            </Marker>
-          ))}
-
-         <button className='btn btn-map' onClick={this.handleAddLocationClick}>
-          Add Your Location
-        </button>
-
-        <div className="map-label" style={{  zIndex: 500 }}>
-
-
-        <div className="row">
-
-          <div className="col-4 d-flex justify-content-end ">
-          <img src={LegendOrange} alt="Fossil tab" className="legend " />
-          </div>
-          <div className="col-8 d-flex justify-content-start ">
-        
-          <h6 className=" map-text  ">OSM Locations</h6>
-            </div>
-        </div>
-
-      <div className="row legend-row">
-
-          <div className="col-4 d-flex justify-content-end ">
-          <img src={LegendBlue} alt="Fossil tab" className="legend " />
-          </div>
-          <div className="col-8 d-flex justify-content-start ">
-        
-          <h6 className=" map-text  ">User Locations</h6>
-
-          </div>
-      </div>
   
 
+    fetchLocations = () => {
+        fetch('/api/get_all_locations')
+        .then(response => response.json())
+        .then(data => {
+            this.setState({ locations: data.locations });
+        })
+        .catch(error => console.error('Error:', error));
+    };
 
-        
+    componentDidMount() {
+        fetch('/api/birdhides_ireland')
+        .then(response => response.json())
+        .then(osmData => {
+            const geojsonData = osmtogeojson(osmData);
+            this.setState({ geojsonData });
+        })
+        .catch(error => console.error('Error:', error));
 
-</div>
-    
-  
-    
-    
-        </MapContainer>
+        this.fetchLocations();
+    }
 
-        {this.state.showModal && (
+    onEachFeature = (feature, layer) => {
+        if (feature.properties && feature.properties.name) {
+            layer.bindPopup(feature.properties.name);
+        }
+    };
+
+    render() {
+        const { geojsonData, locations } = this.state;
+
+        return (
+            <div>
+                <MapContainer center={[53.4129, -8.2439]} zoom={6}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; OpenStreetMap contributors'
+                        className="grayscale-tiles"
+                    />
+                    {geojsonData && (
+                        <GeoJSON
+                            data={geojsonData}
+                            onEachFeature={this.onEachFeature}
+                            style={this.geoJsonStyle}
+                        />
+                    )}
+                    {locations.map((location, index) => (
+                        <Marker key={index} position={[location.latitude, location.longitude]} icon={this.customMarkerIcon}>
+                            <Popup>
+                                <strong>{location.name || 'Unknown Location'}</strong>
+                                <br />
+                                {location.description}
+                                <br />
+                                <button onClick={() => this.handleEditLocation(location)}>Edit</button>
+                                <button onClick={() => this.handleDeleteLocation(location.id)}>Delete</button>
+                            </Popup>
+                        </Marker>
+                    ))}
+                    <button className='btn btn-map' onClick={this.handleAddLocationClick}>Add Your Location</button>
+                    <div className="map-label" style={{  zIndex: 500 }}>
+
+
+                            <div className="row">
+
+                              <div className="col-4 d-flex justify-content-end ">
+                              <img src={LegendOrange} alt="Fossil tab" className="legend " />
+                              </div>
+                              <div className="col-8 d-flex justify-content-start ">
+
+                              <h6 className=" map-text  ">OSM Locations</h6>
+                                </div>
+                            </div>
+
+                            <div className="row legend-row">
+
+                              <div className="col-4 d-flex justify-content-end ">
+                              <img src={LegendBlue} alt="Fossil tab" className="legend " />
+                              </div>
+                              <div className="col-8 d-flex justify-content-start ">
+
+                              <h6 className=" map-text  ">User Locations</h6>
+
+                              </div>
+                            </div>
+
+
+
+
+
+                      </div>
+
+
+
+                </MapContainer>
+
+                {this.state.showModal && (
           <div className="container-fluid ">
             <div className='row'>
               <div className='col-11'>
@@ -245,11 +261,7 @@ submitNewLocation = () => {
           </div>
         )}
 
-
-        <div>
-          
-        </div>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
